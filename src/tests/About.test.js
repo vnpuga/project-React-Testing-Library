77ea3df7aba3410fafb83a075ac00dc0e4e1a2e7
Viviane Puga <vnpuga@gmail.com>
2022-03-22 17:09:20 -0300

    requisito2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,29 @@
-test('', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { About } from '../components';
 
+describe('Teste o componente About', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading', { name: /About Pokédex/i });
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const paragraph1 = screen.getByText(/This application simulates a Pokédex, a/i);
+    const paragraph2 = screen.getByText(/One can filter Pokémons by type, and/i);
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const imgPokedex = screen.getByRole('img');
+
+    expect(imgPokedex).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
