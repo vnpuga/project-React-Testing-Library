4d1f79f5dc83e8bb091ea74f107f8d434e03bf19
Viviane Puga <vnpuga@gmail.com>
2022-03-23 07:38:28 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente FavoritePokemons', () => {
+  it('Teste se é exibido na tela a mensagem "No favorite pokemon found",se a pessoa não'
+    + 'tiver pokémons favoritos', () => {
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(linkFavorite);
+
+    const paragrah = screen.getByText(/No favorite pokemon found/i);
+    expect(paragrah).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(linkMoreDetails);
+
+    const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkbox);
+
+    history.push('/favorites');
+
+    const imgFavoriteCard = screen.getByRole('img', { name: /pikachu sprite/i });
+    expect(imgFavoriteCard).toBeInTheDocument();
+  });
+});
