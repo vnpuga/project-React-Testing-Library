2e43d0e2807a420c115873284387a876d9f7f11e
Viviane Puga <vnpuga@gmail.com>
2022-03-26 15:27:51 -0300

    terminando o requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import { PokemonDetails } from '../components';
 
 describe('Teste o componente PokemonDetails', () => {
   const urlPikachu = '/pokemons/25';
@@ -19,5 +20,41 @@ describe('Teste o componente PokemonDetails', () => {
     expect(linkDetails).not.toBeInTheDocument();
     expect(summary).toBeInTheDocument();
     expect(paragraph).toBeInTheDocument();
+  });
+
+  it('Teste se existe uma seção com os mapas contendo as localizações do pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push(urlPikachu);
+    const titleLocation = screen.getByRole('heading', {
+      name: /game locations of pikachu/i });
+    const imgsLocation = screen.getAllByAltText(/pikachu location/i);
+    const nameLocation1 = screen.getByText(/kanto viridian forest/i);
+    const nameLocation2 = screen.getByText(/kanto power plant/i);
+
+    expect(titleLocation).toBeInTheDocument();
+    expect(imgsLocation.length).toBe(2);
+    expect(nameLocation1).toBeInTheDocument();
+    expect(nameLocation2).toBeInTheDocument();
+    expect(imgsLocation[0]).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(imgsLocation[1]).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(imgsLocation[0]).toHaveAttribute('alt', 'Pikachu location');
+    expect(imgsLocation[1]).toHaveAttribute('alt', 'Pikachu location');
+  });
+
+  it('Teste se o usuário pode favoritar um pokémon através da página de detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push(urlPikachu);
+    const labelFavorito = screen.getByLabelText('Pokémon favoritado?');
+    expect(labelFavorito).toBeInTheDocument();
+
+    const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkbox);
+    const checked = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(checked).toBeInTheDocument();
+
+    userEvent.click(checkbox);
+    expect(checked).not.toBeInTheDocument();
   });
 });
